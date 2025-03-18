4fe3a3aec08da0f8b16bbee4a39328453e87d142
ShiromiAnthonythas <shiromii2002@gmail.com>
2025-03-18 21:44:41 +0530

    Add files via upload

---
+++ 2021ict86Day1.js
@@ -0,0 +1,77 @@
+//print hello
+      console.log("hello world")
+  
+  //variable define
+  let num1=30;
+  let num2=20;
+  
+  let sum=num1+num2;
+  let sub=num1-num2;
+  let mul=num1*num2;
+  let div=num1/num2;
+  
+  console.log("sum:"+sum)
+  console.log("sub:"+sub)
+  console.log("mul:"+mul)
+  console.log("div:"+div)
+  
+  var x=5
+  var y=20
+  
+  var sum=num1+num2;
+  var sub=num1-num2;
+  var mul=num1*num2;
+  var div=num1/num2;
+  
+  console.log("sum:"+sum)
+  console.log("sub:"+sub)
+  console.log("mul:"+mul)
+  console.log("div:"+div)
+  
+  const z=18  //did not change
+  
+  //print number from 1 to 10
+  for(var i=1;i<=10;i++)
+  {
+	  console.log(i)
+  }
+   console.log("") 
+   
+  //print only the odd number
+   for(var i=1;i<=10;i++)
+  {
+	  if(i%2==1){
+	  console.log(i)
+	  }
+	  else {
+		  continue;
+	  }
+  }
+  
+  console.log("")
+  //print the numbers in reverse
+   for(var i=10;i>=0;i--)
+  {
+	 console.log(i)
+  }	
+ console.log("")
+ 
+ //reverse the numbers in left and right 4321-5-9876
+ 
+ var i=console.log(parseInt(9/2));
+ var a=i-1
+ while(a>0)
+ {
+	 console.log(a);
+	 a--;
+ }
+ console.log(parseInt(9/2));
+ 
+ 
+ var j=n;
+ while(j>i)
+ {
+	 console.log(j)
+	 j--;
+ }
+ 
